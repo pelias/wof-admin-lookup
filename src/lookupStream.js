4dc3571d07aea35b7a08af0abac7395b527578bd
Stephen Hess <stephen.k.hess@gmail.com>
2016-01-15 17:27:39 -0500

    added support for resolver returning multiple values to lookupStream

--- src/lookupStream.js
+++ src/lookupStream.js
@@ -1,6 +1,12 @@
 var through2 = require('through2');
 var _ = require('lodash');
 
+function addAll(values, doc, placetype) {
+  values.forEach(function(value) {
+    doc.addParent(placetype, value.name, value.id.toString());
+  });
+}
+
 function createLookupStream(resolver) {
   return through2.obj(function(doc, enc, callback) {
     // don't do anything if there's no centroid
@@ -14,22 +20,28 @@ function createLookupStream(resolver) {
       }
 
       if (!_.isUndefined(result.country)) {
-        doc.setAdmin( 'admin0', result.country);
+        doc.setAdmin( 'admin0', result.country[0].name);
+        addAll(result.country, doc, 'country');
       }
       if (!_.isUndefined(result.region)) {
-        doc.setAdmin( 'admin1', result.region);
+        doc.setAdmin( 'admin1', result.region[0].name);
+        addAll(result.region, doc, 'region');
       }
       if (!_.isUndefined(result.county)) {
-        doc.setAdmin( 'admin2', result.county);
+        doc.setAdmin( 'admin2', result.county[0].name);
+        addAll(result.county, doc, 'county');
       }
       if (!_.isUndefined(result.locality)) {
-        doc.setAdmin( 'locality', result.locality);
+        doc.setAdmin( 'locality', result.locality[0].name);
+        addAll(result.locality, doc, 'locality');
       }
       if (!_.isUndefined(result.localadmin)) {
-        doc.setAdmin( 'local_admin', result.localadmin);
+        doc.setAdmin( 'local_admin', result.localadmin[0].name);
+        addAll(result.localadmin, doc, 'localadmin');
       }
       if (!_.isUndefined(result.neighbourhood)) {
-        doc.setAdmin( 'neighborhood', result.neighbourhood);
+        doc.setAdmin( 'neighborhood', result.neighbourhood[0].name);
+        addAll(result.neighbourhood, doc, 'neighbourhood');
       }
 
       callback(null, doc);
--- test/lookupStreamTest.js
+++ test/lookupStreamTest.js
@@ -15,7 +15,7 @@ function test_stream(input, testedStream, callback) {
 tape('tests', function(test) {
   test.test('doc without centroid should not modify input', function(t) {
     var input = [
-      new Document( 'whosonfirst', '1')
+      new Document( 'whosonfirst', 'placetype', '1')
     ];
 
     var lookupStream = stream.createLookupStream();
@@ -29,28 +29,58 @@ tape('tests', function(test) {
 
   test.test('country, region, county, locality, and neighborhood fields should be set into document', function(t) {
     var input = [
-      new Document( 'whosonfirst', '1').setCentroid({ lat: 12.121212, lon: 21.212121 })
+      new Document( 'whosonfirst', 'placetype', '1').setCentroid({ lat: 12.121212, lon: 21.212121 })
     ];
 
     var expected = [
-      new Document( 'whosonfirst', '1')
+      new Document( 'whosonfirst', 'placetype', '1')
         .setCentroid({ lat: 12.121212, lon: 21.212121 })
-        .setAdmin( 'admin0', 'Country')
-        .setAdmin( 'admin1', 'Region')
-        .setAdmin( 'admin2', 'County')
-        .setAdmin( 'locality', 'Locality')
-        .setAdmin( 'local_admin', 'LocalAdmin')
-        .setAdmin( 'neighborhood', 'Neighbourhood')
+        .setAdmin( 'admin0', 'Country 1')
+        .addParent('country', 'Country 1', '1')
+        .addParent('country', 'Country 2', '2')
+        .setAdmin( 'admin1', 'Region 1')
+        .addParent('region', 'Region 1', '3')
+        .addParent('region', 'Region 2', '4')
+        .setAdmin( 'admin2', 'County 1')
+        .addParent('county', 'County 1', '5')
+        .addParent('county', 'County 2', '6')
+        .setAdmin( 'locality', 'Locality 1')
+        .addParent('locality', 'Locality 1', '7')
+        .addParent('locality', 'Locality 2', '8')
+        .setAdmin( 'local_admin', 'LocalAdmin 1')
+        .addParent('localadmin', 'LocalAdmin 1', '9')
+        .addParent('localadmin', 'LocalAdmin 2', '10')
+        .setAdmin( 'neighborhood', 'Neighbourhood 1')
+        .addParent('neighbourhood', 'Neighbourhood 1', '11')
+        .addParent('neighbourhood', 'Neighbourhood 2', '12')
     ];
 
     var resolver = function(centroid, callback) {
       var result = {
-        country: 'Country',
-        region: 'Region',
-        county: 'County',
-        locality: 'Locality',
-        localadmin: 'LocalAdmin',
-        neighbourhood: 'Neighbourhood'
+        country: [
+          { id: 1, name: 'Country 1'},
+          { id: 2, name: 'Country 2'}
+        ],
+        region: [
+          { id: 3, name: 'Region 1'},
+          { id: 4, name: 'Region 2'}
+        ],
+        county: [
+          { id: 5, name: 'County 1'},
+          { id: 6, name: 'County 2'}
+        ],
+        locality: [
+          { id: 7, name: 'Locality 1'},
+          { id: 8, name: 'Locality 2'}
+        ],
+        localadmin: [
+          { id: 9, name: 'LocalAdmin 1'},
+          { id: 10, name: 'LocalAdmin 2'}
+        ],
+        neighbourhood: [
+          { id: 11, name: 'Neighbourhood 1'},
+          { id: 12, name: 'Neighbourhood 2'}
+        ]
       };
 
       setTimeout(callback, 0, null, result);
@@ -68,26 +98,28 @@ tape('tests', function(test) {
 
   test.test('resolver with field-less result should only set fields that are present', function(t) {
     var input = [
-      new Document( 'whosonfirst', '1')
+      new Document( 'whosonfirst', 'placetype', '1')
         .setCentroid({ lat: 12.121212, lon: 21.212121 }),
-      new Document( 'whosonfirst', '1')
+      new Document( 'whosonfirst', 'placetype', '1')
         .setCentroid({ lat: 13.131313, lon: 31.313131 })
     ];
 
     var expected = [
-      new Document( 'whosonfirst', '1')
+      new Document( 'whosonfirst', 'placetype', '1')
         .setCentroid({ lat: 12.121212, lon: 21.212121 })
-        .setAdmin( 'admin1', 'Region'),
-      new Document( 'whosonfirst', '1')
+        .setAdmin( 'admin1', 'Region')
+        .addParent('region', 'Region', '1'),
+      new Document( 'whosonfirst', 'placetype', '1')
         .setCentroid({ lat: 13.131313, lon: 31.313131 })
         .setAdmin( 'admin0', 'Country')
+        .addParent('country', 'Country', '2')
     ];
 
     var resolver = function(centroid, callback) {
       if (_.isEqual(centroid, { lat: 12.121212, lon: 21.212121 } )) {
-        setTimeout(callback, 0, null, { region: 'Region' });
+        setTimeout(callback, 0, null, { region: [ { id: 1, name: 'Region' } ] });
       } else if (_.isEqual(centroid, { lat: 13.131313, lon: 31.313131 })) {
-        setTimeout(callback, 0, null, { country: 'Country' });
+        setTimeout(callback, 0, null, { country: [ {id: 2, name: 'Country' } ] });
       }
 
     };
@@ -102,10 +134,10 @@ tape('tests', function(test) {
   });
 
   test.test('resolver throwing error should push doc onto stream unmodified', function(t) {
-    var input = new Document( 'whosonfirst', '1')
+    var input = new Document( 'whosonfirst', 'placetype', '1')
         .setCentroid({ lat: 12.121212, lon: 21.212121 });
 
-    var expected = new Document( 'whosonfirst', '1')
+    var expected = new Document( 'whosonfirst', 'placetype', '1')
         .setCentroid({ lat: 12.121212, lon: 21.212121 });
 
     var resolver = function(centroid, callback) {
