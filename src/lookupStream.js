ef5f71d61a0aa0ae91df92af5c7c6e97c9e36e50
Stephen Hess <stephen.k.hess@gmail.com>
2016-01-19 14:08:46 -0500

    added `regions` map, not tied in yet

--- src/lookupStream.js
+++ src/lookupStream.js
@@ -7,6 +7,76 @@ function addAll(values, doc, placetype) {
   });
 }
 
+// only US is currently supported
+// to support more countries, add them in a similar fashion
+var regions = {
+  'United States': {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'Washington, D.C.': 'DC',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
+  }
+};
+
+regions.isSupportedCountry = function(country) {
+  return this.hasOwnProperty(country);
+};
+
+regions.isSupportedRegion = function(country, name) {
+  return this.hasOwnProperty(country) && this[country].hasOwnProperty(name);
+};
+
+regions.getAbbreviation = function(country, name) {
+  return this[country][name];
+};
+
 function createLookupStream(resolver) {
   return through2.obj(function(doc, enc, callback) {
     // don't do anything if there's no centroid
