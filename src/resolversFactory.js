92034ed41da92b2e280ba3d56b3205c49d071fef
Diana Shkolnikov <dianashk@gmail.com>
2016-01-25 17:16:41 -0500

    Move pelias-model to devDependencies again, stop using http.globalAgent in favor of local instance

--- src/resolversFactory.js
+++ src/resolversFactory.js
@@ -1,5 +1,6 @@
 var util = require('util');
 var http = require('http');
+var request = require('request');
 var peliasConfig = require( 'pelias-config' ).generate();
 
 
@@ -11,37 +12,41 @@ function createWofPipResolver(url, config) {
     maxConcurrentReqs = config.imports.adminLookup.maxConcurrentReqs;
   }
 
-  http.globalAgent.maxSockets = maxConcurrentReqs;
+  var httpAgent = new http.Agent({
+    keepAlive: true,
+    maxSockets: maxConcurrentReqs
+  });
 
   return function(centroid, callback) {
-    var requestUrl = util.format('%s/?latitude=%d&longitude=%d', url, centroid.lat, centroid.lon);
+    var urlPath = util.format('%s/?latitude=%d&longitude=%d', url, centroid.lat, centroid.lon);
 
-    http.get(requestUrl, function(response) {
-      var contents = '';
+    var options = {
+      method: 'GET',
+      url: urlPath,
+      agent: httpAgent
+    };
 
-      response.setEncoding('utf8');
-      response.on('data', function(data) { contents += data; } );
-      response.on('end', function() {
-        // convert the array to an object keyed on the array element's Placetype field
-        var result = JSON.parse(contents).reduce(function(obj, elem) {
-          if (!obj.hasOwnProperty(elem.Placetype)) {
-            obj[elem.Placetype] = [];
-          }
-          obj[elem.Placetype].push({
-            id: elem.Id,
-            name: elem.Name
-          });
-          return obj;
-        }, {});
+    request(options, function (err, res, body) {
+      if (err) {
+        console.log(err.stack);
+        return callback(err, null);
+      }
 
-        return callback(null, result);
+      // convert the array to an object keyed on the array element's Placetype field
+      var result = JSON.parse(body).reduce(function (obj, elem) {
+        if (!obj.hasOwnProperty(elem.Placetype)) {
+          obj[elem.Placetype] = [];
+        }
+        obj[elem.Placetype].push({
+          id: elem.Id,
+          name: elem.Name
+        });
+        return obj;
+      }, {});
 
-      });
+      return callback(null, result);
 
-    }).on('error', function(err) {
-      return callback(err, null);
     });
-
   };
 
 }
