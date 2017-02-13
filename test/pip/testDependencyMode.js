051f98bc998dc2be32b208a19fd8f3502e8a21fc
Stephen Hess <stephen.k.hess@gmail.com>
2017-02-13 11:38:30 -0500

    added functional tests from wof-pip-service (and fixed them)

---
+++ test/pip/testDependencyMode.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const config = require('pelias-config').generate();
+
+const createPIPService = require('../../src/pip/index').create;
+const fs = require('fs');
+const async = require('async');
+const _ = require('lodash');
+
+let inputData = require('./testData.json');
+
+const layers = [
+  //'continent',
+  'country', // 216
+  'county', // 18166
+  'dependency', // 39
+  'disputed', // 39
+  'localadmin', // 106880
+  'locality', // 160372
+  'macrocounty', // 350
+  'macroregion', // 82
+  'neighbourhood', // 62936
+  'region' // 4698
+];
+
+function test(callback) {
+  let startTime = process.hrtime();
+
+  createPIPService(config.imports.whosonfirst.datapath, layers, (err, pipService) => {
+
+    console.log(`Total load time (minutes) = ${(getMicroSeconds(process.hrtime(startTime))/1000000/60)}`);
+
+    inputData = _.concat(inputData, _.clone(inputData));
+    inputData = _.concat(inputData, _.clone(inputData));
+    inputData = _.concat(inputData, _.clone(inputData));
+
+    startTime = process.hrtime();
+
+    async.forEach(inputData, (location, done) => {
+      pipService.lookup(location.latitude, location.longitude, layers, (err, results) => {
+        location.results = results;
+        done();
+      });
+    },
+    () => {
+      const reqDuration = getMicroSeconds(process.hrtime(startTime)) / inputData.length;
+      console.log(`Query count = ${inputData.length}`);
+      console.log(`Average duration (μsec) = ${reqDuration}`);
+      console.log(`Computed req/sec = ${1000000 / reqDuration}`);
+
+      fs.writeFileSync('./actualTestResults.json', JSON.stringify(inputData, null, 2));
+
+      pipService.end();
+
+      callback();
+    });
+
+  });
+}
+
+function getMicroSeconds(time) {
+  return (time[0] * 1e9 + time[1]) / 1000;
+}
+
+test((err) => {
+  process.exit(err);
+});
---
+++ test/pip/testLayerLookup.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const config = require('pelias-config').generate();
+
+const fs = require('fs');
+const path = require( 'path' );
+
+const async = require('async');
+const deep = require( 'deep-diff' );
+
+const createPIPService = require('../../src/pip/index').create;
+
+/*
+ * Only run some layers to speed up the tests
+ */
+const layers = [
+  //'continent',
+  'country', // 216
+  //'county', // 18166
+  'dependency', // 39
+  'disputed', // 39
+  //'localadmin', // 106880
+  //'locality', // 160372
+  'macrocounty', // 350
+  'macroregion', // 82
+  //'neighbourhood', // 62936
+  'region' // 4698
+];
+
+function test(callback) {
+  createPIPService(config.imports.whosonfirst.datapath, layers, (err, pipService) => {
+    const basePath = path.resolve(__dirname);
+    const inputDataPath = path.join(basePath, 'data', 'layerTestData.json');
+    const inputData = require( inputDataPath );
+    const results = [];
+    const expectedPath = path.join(basePath, 'data', 'expectedLayerTestResults.json');
+
+    async.forEach(inputData, (location, done) => {
+        pipService.lookup(location.latitude, location.longitude, location.layers, (err, result) => {
+          results.push(result);
+          done();
+        });
+      },
+      () => {
+        const expected = JSON.parse(fs.readFileSync(expectedPath));
+
+        sortResults(expected);
+        sortResults(results);
+
+        // uncomment this to write the actual results to the expected file
+        // make sure they look ok though. semicolon left off so jshint reminds you
+        // not to commit this line
+        // fs.writeFileSync(expectedPath, JSON.stringify(results, null, 2))
+
+        const diff = deep(expected, results);
+
+        pipService.end();
+
+        if (diff) {
+          console.log('expected and actual output are different');
+          console.log(diff);
+        }
+        else {
+          console.log('expected and actual output are the same');
+        }
+        callback();
+      });
+  });
+}
+
+/**
+ * Sort result arrays which look like this
+ *
+ * [ [{Id:1}, {Id:2}, {Id:3}], [{Id:5}, {Id:6], [{Id:7}, {Id:8}, {Id:9}] ]
+ */
+function sortResults(results) {
+  // sort the individual result arrays
+  results.forEach((a) => {
+    a.sort((a, b) => {
+      return b.Id - a.Id;
+    });
+  });
+
+  results.sort((a, b) => {
+    return b[0].Id - a[0].Id;
+  });
+
+  return results;
+}
+
+test((err) => {
+  process.exit(err);
+});
