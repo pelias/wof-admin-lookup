16277f75a1f4db50bf1a76a16cc8f4d3dd0360e7
Stephen Hess <stephen.k.hess@gmail.com>
2017-02-28 15:37:41 -0500

    converted loadJSON to parallelStream for faster loading

--- src/pip/components/loadJSON.js
+++ src/pip/components/loadJSON.js
@@ -1,20 +1,29 @@
-var path = require('path');
-var through = require('through2');
-var logger = require( 'pelias-logger' ).get( 'wof-pip-service:loadJSON' );
-var fs = require('fs');
+const path = require('path');
+const fs = require('fs');
+const parallelStream = require('pelias-parallel-stream');
 
-module.exports.create = function(datapath) {
-  // parse and return JSON contents
-  return through.obj(function(record, enc, next) {
-    var filename = [ datapath, 'data', record.path ].join(path.sep);
+const maxInFlight = 10;
 
-    try {
-      this.push(JSON.parse(fs.readFileSync(filename)));
-    }
-    catch (e) {
-      logger.error('exception occured parsing ' + filename + ': ' + e);
-    }
-
-    next();
+module.exports.create = (datapath) => {
+  return parallelStream(maxInFlight, function(record, enc, next) {
+    const filename = [ datapath, 'data', record.path ].join(path.sep);
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        console.error(`exception reading file ${filename}`);
+        next(err);
+      } else {
+        try {
+          const object = JSON.parse(data);
+          next(null, object);
+        } catch (parse_err) {
+          console.error(`exception parsing JSON in file ${record.path}: ${parse_err}`);
+          console.error('Inability to parse JSON usually means that WOF has been cloned ' +
+                        'without using git-lfs, please see instructions here: ' +
+                        'https://github.com/whosonfirst/whosonfirst-data#git-and-large-files');
+          next(parse_err);
+        }
+      }
+    });
   });
+
 };
