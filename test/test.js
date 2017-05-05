546ac08de54ead6b7baa73cabf26547b79084b8b
Stephen Hess <stephen.k.hess@gmail.com>
2017-05-05 00:39:08 -0400

    switched to pelias-whosonfirst common streams

--- test/test.js
+++ test/test.js
@@ -4,8 +4,5 @@ require ('./lookupStreamTest.js');
 require ('./localPipResolverTest.js');
 require ('./pip/components/extractFieldsTest.js');
 require ('./pip/components/extractFieldsLocalizedNameTest.js');
-require ('./pip/components/loadJSONTest.js');
 require ('./pip/components/simplifyGeometryTest.js');
-require ('./pip/components/isActiveRecordTest.js');
-require ('./pip/components/filterOutNamelessRecordsTest.js');
 require ('./pip/components/filterOutUnimportantRecordsTest.js');
