1bc80a2cb8b300c4b80f410ecba598b17e49dae7
Stephen Hess <stephen.k.hess@gmail.com>
2017-09-01 12:17:33 -0400

    added tests for worker

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-// require ('./schema.js');
-// require ('./index.js');
-// require ('./lookupStreamTest.js');
-// require ('./localPipResolverTest.js');
+require ('./schema.js');
+require ('./index.js');
+require ('./lookupStreamTest.js');
+require ('./localPipResolverTest.js');
+require ('./pip/worker.js');
 require ('./pip/components/extractFieldsTest.js');
 require ('./pip/components/extractFieldsLocalizedNameTest.js');
-// require ('./pip/components/simplifyGeometryTest.js');
-// require ('./pip/components/filterOutUnimportantRecordsTest.js');
-// require ('./pip/components/filterOutPointRecordsTest.js');
+require ('./pip/components/simplifyGeometryTest.js');
+require ('./pip/components/filterOutUnimportantRecordsTest.js');
+require ('./pip/components/filterOutPointRecordsTest.js');
