82e9b89202a38740f7634622aeb1797fdad4c8fa
Sergey Zelenov <sergey.zelenov@moovel.com>
2017-10-13 17:28:17 +0200

    Added tests for remote pipResolver;

---
+++ src/remotePipResolver.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const logger = require('pelias-logger').get('wof-admin-lookup');
+const _ = require('lodash');
+const request = require('request');
+
+/**
+ * RemotePIPService class
+ *
+ * @param {string} [url] url to pip service
+ * @constructor
+ */
+function RemotePIPService(url) {
+  this.pipServiceURL = url;
+}
+
+/**
+ * @param {object} centroid
+ * @param {array} _ - previously search_layers. it's not using anymore, but stays here for backward compatibility
+ * @param callback
+ */
+RemotePIPService.prototype.lookup = function lookup(centroid, _, callback) {
+
+  const options = {
+    uri: `${this.pipServiceURL}/${centroid.lon}/${centroid.lat}`,
+    method: 'GET',
+    json: true
+  };
+
+  request(options, (err, response, results) => {
+    if (err) {
+      return callback(err.message);
+    }
+
+    if (response.statusCode !== 200) {
+      return callback(results);
+    }
+
+    callback(null, results);
+  });
+};
+
+/**
+ * Factory function
+ *
+ * @param {string} [url]
+ * @returns {RemotePIPService}
+ */
+module.exports = (url) => {
+  return new RemotePIPService(url);
+};
