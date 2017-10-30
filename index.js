efd31ff99fcd76276dc8c2dbf8b789177686f302
Julian Simioni <julian@mapzen.com>
2017-10-30 16:14:50 -0400

    Define functions separately from module.exports
    This makes it easier for functions to reference each other and reduces
    levels of indentation

--- index.js
+++ index.js
@@ -6,27 +6,28 @@ const through = require('through2');
 const _ = require('lodash');
 const os = require('os');
 
-module.exports = {
-  create: (layers) => {
-    if (peliasConfig.imports.adminLookup.enabled) {
-      const resolver = module.exports.resolver(layers);
-
-      return require('./src/lookupStream')(resolver,
-        peliasConfig.imports.adminLookup.maxConcurrentReqs);
-    } else {
-      return through.obj();
-    }
-  },
-
-  resolver: (layers) => {
-    let resolver;
-    if (_.has(peliasConfig, 'imports.services.pip.url')) {
-      resolver = require('./src/remotePipResolver')(peliasConfig.imports.services.pip.url);
-    } else {
-      const datapath = peliasConfig.imports.whosonfirst.datapath;
-      resolver = require('./src/localPipResolver')(datapath, layers);
-    }
-
-    return resolver;
+function create(layers) {
+  if (peliasConfig.imports.adminLookup.enabled) {
+    return require('./src/lookupStream')(resolver(layers),
+      peliasConfig.imports.adminLookup.maxConcurrentReqs);
+  } else {
+    return through.obj();
   }
+}
+
+function resolver(layers) {
+  let resolver;
+  if (_.has(peliasConfig, 'imports.services.pip.url')) {
+    resolver = require('./src/remotePipResolver')(peliasConfig.imports.services.pip.url);
+  } else {
+    const datapath = peliasConfig.imports.whosonfirst.datapath;
+    resolver = require('./src/localPipResolver')(datapath, layers);
+  }
+
+  return resolver;
+}
+
+module.exports = {
+  create: create,
+  resolver: resolver
 };
