bc56b77e2f3c6efa30644cfb2b0d8998a468186d
Julian Simioni <julian@mapzen.com>
2017-10-30 16:12:45 -0400

    Use resolver method to further reduce duplication

--- index.js
+++ index.js
@@ -9,24 +9,24 @@ const os = require('os');
 module.exports = {
   create: (layers) => {
     if (peliasConfig.imports.adminLookup.enabled) {
-      let resolver;
-      if (_.has(peliasConfig, 'imports.services.pip.url')) {
-        resolver = require('./src/remotePipResolver')(peliasConfig.imports.services.pip.url);
-      } else {
-        const datapath = peliasConfig.imports.whosonfirst.datapath;
-       resolver = require('./src/localPipResolver')(datapath, layers);
-      }
+      const resolver = module.exports.resolver(layers);
 
       return require('./src/lookupStream')(resolver,
         peliasConfig.imports.adminLookup.maxConcurrentReqs);
     } else {
       return through.obj();
     }
-
   },
-  resolver: (datapath) => {
-    const resolver = require('./src/localPipResolver')(
-      datapath || peliasConfig.imports.whosonfirst.datapath);
+
+  resolver: (layers) => {
+    let resolver;
+    if (_.has(peliasConfig, 'imports.services.pip.url')) {
+      resolver = require('./src/remotePipResolver')(peliasConfig.imports.services.pip.url);
+    } else {
+      const datapath = peliasConfig.imports.whosonfirst.datapath;
+      resolver = require('./src/localPipResolver')(datapath, layers);
+    }
+
     return resolver;
   }
 };
