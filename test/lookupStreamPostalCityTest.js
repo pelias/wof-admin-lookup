5edaf2d0bf55847e5c2c4477802a2348aad59fa3
Peter Johnson <insomnia@rcpt.at>
2018-09-06 15:55:16 +0200

    feat(postal_cities): test cases

--- test/lookupStreamPostalCityTest.js
+++ test/lookupStreamPostalCityTest.js
@@ -74,6 +74,68 @@ tape('tests', (test) => {
     });
   });
 
+  test.test('postal cities - USA lookup - ZIP 17033', (t) => {
+    const inputDoc = new Document( 'whosonfirst', 'placetype', '1')
+      .setCentroid({ lat: 12.121212, lon: 21.212121 })
+      .setAddress('zip', '17033');
+
+    const expectedDoc = new Document( 'whosonfirst', 'placetype', '1')
+      .setCentroid({ lat: 12.121212, lon: 21.212121 })
+      .addParent( 'country', 'United States', '1', 'USA')
+      .addParent( 'locality', 'Hershey', '101719813', undefined)
+      .addParent( 'locality', 'Locality 1', '2', 'ABC')
+      .setAddress('zip', '17033');
+
+    const resolver = {
+      lookup: (centroid, search_layers, callback) => {
+        const result = {
+          country: [ {id: 1, name: 'United States', abbr: 'USA'} ],
+          locality: [ {id: 2, name: 'Locality 1', abbr: 'ABC'} ]
+        };
+        setTimeout(callback, 0, null, result);
+      }
+    };
+
+    const lookupStream = stream(resolver);
+    t.doesNotThrow(() => {
+      test_stream([inputDoc], lookupStream, (err, actual) => {
+        t.deepEqual(actual, [expectedDoc], 'locality should be replaced with postal city');
+        t.end();
+      });
+    });
+  });
+
+  test.test('postal cities - USA lookup - ZIP 17036', (t) => {
+    const inputDoc = new Document( 'whosonfirst', 'placetype', '1')
+      .setCentroid({ lat: 12.121212, lon: 21.212121 })
+      .setAddress('zip', '17036');
+
+    const expectedDoc = new Document( 'whosonfirst', 'placetype', '1')
+      .setCentroid({ lat: 12.121212, lon: 21.212121 })
+      .addParent( 'country', 'United States', '1', 'USA')
+      .addParent( 'locality', 'Hummelstown', '101717689', undefined)
+      .addParent( 'locality', 'Locality 1', '2', 'ABC')
+      .setAddress('zip', '17036');
+
+    const resolver = {
+      lookup: (centroid, search_layers, callback) => {
+        const result = {
+          country: [ {id: 1, name: 'United States', abbr: 'USA'} ],
+          locality: [ {id: 2, name: 'Locality 1', abbr: 'ABC'} ]
+        };
+        setTimeout(callback, 0, null, result);
+      }
+    };
+
+    const lookupStream = stream(resolver);
+    t.doesNotThrow(() => {
+      test_stream([inputDoc], lookupStream, (err, actual) => {
+        t.deepEqual(actual, [expectedDoc], 'locality should be replaced with postal city');
+        t.end();
+      });
+    });
+  });
+
   test.test('postal cities - USA lookup - ZIP 95616 - no PIP locality', (t) => {
     const inputDoc = new Document( 'whosonfirst', 'placetype', '1')
       .setCentroid({ lat: 12.121212, lon: 21.212121 })
