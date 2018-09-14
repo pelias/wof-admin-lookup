d80d6a1746f3d66cbf4f5a4cfac5a9ce0740c5cf
Peter Johnson <insomnia@rcpt.at>
2018-09-14 12:10:32 +0200

    feat(postal_cities): add config based off OSM, update data, code and tests

--- src/usePostalCity.js
+++ src/usePostalCity.js
@@ -27,58 +27,72 @@ function usePostalCity( result, doc ){
   if( !countrycode || countrycode.length !== 3 ){ return; }
 
   // look up the 'postal city' locality
-  const locality = postalCityMap.lookup(countrycode, postalcode);
+  var localities = postalCityMap.lookup(countrycode, postalcode);
 
-  // check the locality is valid
-  if( !locality ){ return; }
+  // check the localities list is valid
+  if( !Array.isArray(localities) || !localities.length ){ return; }
 
-  // we will use it instead of the PIP locality
+  // we will use the first value instead of the PIP locality
   try {
 
     // save the existing locality as an alias
     // note: this would be better using getters, but the model does not
     // currently offer a 'getParent' method.
-    var pipLocality;
     if( doc.parent.hasOwnProperty('locality') && doc.parent.locality.length &&
         doc.parent.hasOwnProperty('locality_id') && doc.parent.locality_id.length &&
         doc.parent.hasOwnProperty('locality_a') && doc.parent.locality_a.length ){
-      pipLocality = {
-        name: doc.parent.locality[0],
-        wofid: doc.parent.locality_id[0],
-        abbr: doc.parent.locality_a[0],
-      };
+
+      // append the existing localities on to the postal city localities array
+      localities = localities.concat(
+        doc.parent.locality.map((_, i) => {
+          return {
+            name:   doc.parent.locality[i],
+            wofid:  doc.parent.locality_id[i],
+            abbr:   doc.parent.locality_a[i]
+          };
+        })
+      );
     }
 
-    // remove the existing locality
+    // remove the existing locality info
     doc.clearParent('locality');
 
-    // addParent can throw an error if, for example, name is an empty string
-    doc.addParent('locality', locality.name, locality.wofid, locality.abbr);
+    // deduplicate localities
+    localities = localities.filter((locality, index, self) => {
+      return index === self.findIndex(t => t.wofid === locality.wofid);
+    });
 
-    // add the original PIP locality as an alias
-    if( pipLocality ){
-      doc.addParent('locality', pipLocality.name, pipLocality.wofid, pipLocality.abbr);
-    }
+    // add all localities, using the first matching postal locality as the default
+    // and then indexing all other localities as aliases (including any existing localities)
+    localities.forEach(locality => {
+
+      // note: addParent can throw an error if, for example, name is an empty string
+      doc.addParent('locality', locality.name, locality.wofid, locality.abbr);
+    });
   }
   catch (err) {
     logger.info('invalid value', {
       centroid: doc.getCentroid(),
       result: {
         type: 'locality',
-        values: [locality]
+        values: localities
       }
     });
   }
 }
 
 function getPostalCode(doc){
-  return (doc.getAddress('zip') || '').toString().replace(/\s/g, '');
+  return (doc.getAddress('zip') || '').toString();
 }
 
 function getCountryCode(result){
-  if( Array.isArray(result.country) && result.country.length ){
-    return (result.country[0].abbr || '').toUpperCase();
+  if( Array.isArray(result.dependency) && result.dependency.length && result.dependency[0].abbr ){
+    return result.dependency[0].abbr.toUpperCase();
   }
+  if( Array.isArray(result.country) && result.country.length && result.country[0].abbr ){
+    return result.country[0].abbr.toUpperCase();
+  }
+  return '';
 }
 
 module.exports = usePostalCity;
