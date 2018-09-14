be20ddf2857fbf07fd9450d54ef1b0070386a84b
Peter Johnson <insomnia@rcpt.at>
2018-09-14 12:10:32 +0200

    feat(postal_cities): add config based off OSM, update data, code and tests

--- test/lookupStreamPostalCityTest.js
+++ test/lookupStreamPostalCityTest.js
@@ -52,6 +52,8 @@ tape('tests', (test) => {
       .setCentroid({ lat: 12.121212, lon: 21.212121 })
       .addParent( 'country', 'United States', '1', 'USA')
       .addParent( 'locality', 'Nottingham', '1125996559', undefined)
+      .addParent( 'locality', 'Baltimore', '85949461', undefined)
+      .addParent( 'locality', 'White Marsh', '85950229', undefined)
       .addParent( 'locality', 'Locality 1', '2', 'ABC')
       .setAddress('zip', '21236');
 
@@ -114,6 +116,7 @@ tape('tests', (test) => {
       .setCentroid({ lat: 12.121212, lon: 21.212121 })
       .addParent( 'country', 'United States', '1', 'USA')
       .addParent( 'locality', 'Hummelstown', '101717689', undefined)
+      .addParent( 'locality', 'Hershey', '101719813', undefined)
       .addParent( 'locality', 'Locality 1', '2', 'ABC')
       .setAddress('zip', '17036');
 
@@ -151,6 +154,100 @@ tape('tests', (test) => {
       lookup: (centroid, search_layers, callback) => {
         const result = {
           country: [ {id: 1, name: 'United States', abbr: 'USA'} ]
+        };
+        setTimeout(callback, 0, null, result);
+      }
+    };
+
+    const lookupStream = stream(resolver);
+    t.doesNotThrow(() => {
+      test_stream([inputDoc], lookupStream, (err, actual) => {
+        t.deepEqual(actual, [expectedDoc], 'locality should be replaced with postal city');
+        t.end();
+      });
+    });
+  });
+
+  test.test('postal cities - USA lookup - ZIP 94610 - including state prefix and ZIP+4', (t) => {
+    const inputDoc = new Document( 'whosonfirst', 'placetype', '1')
+      .setCentroid({ lat: 12.121212, lon: 21.212121 })
+      .setAddress('zip', 'CA 94610-2737');
+
+    const expectedDoc = new Document( 'whosonfirst', 'placetype', '1')
+      .setCentroid({ lat: 12.121212, lon: 21.212121 })
+      .addParent( 'country', 'United States', '1', 'USA')
+      .addParent( 'locality', 'Piedmont', '85921877', undefined)
+      .addParent( 'locality', 'Oakland', '85921881', undefined)
+      .setAddress('zip', 'CA 94610-2737');
+
+    const resolver = {
+      lookup: (centroid, search_layers, callback) => {
+        const result = {
+          country: [ {id: 1, name: 'United States', abbr: 'USA'} ]
+        };
+        setTimeout(callback, 0, null, result);
+      }
+    };
+
+    const lookupStream = stream(resolver);
+    t.doesNotThrow(() => {
+      test_stream([inputDoc], lookupStream, (err, actual) => {
+        t.deepEqual(actual, [expectedDoc], 'locality should be replaced with postal city');
+        t.end();
+      });
+    });
+  });
+
+  test.test('postal cities - AUS lookup - postcode 2617', (t) => {
+    const inputDoc = new Document( 'whosonfirst', 'placetype', '1')
+      .setCentroid({ lat: 12.121212, lon: 21.212121 })
+      .setAddress('zip', '2617');
+
+    const expectedDoc = new Document( 'whosonfirst', 'placetype', '1')
+      .setCentroid({ lat: 12.121212, lon: 21.212121 })
+      .addParent( 'country', 'Australia', '1', 'AUS')
+      .addParent( 'locality', 'Bruce', '101938721', undefined)
+      .addParent( 'locality', 'Belconnen', '101938713', undefined)
+      .addParent( 'locality', 'Evatt', '101938763', undefined)
+      .addParent( 'locality', 'Giralang', '101938789', undefined)
+      .setAddress('zip', '2617');
+
+    const resolver = {
+      lookup: (centroid, search_layers, callback) => {
+        const result = {
+          country: [ {id: 1, name: 'Australia', abbr: 'AUS'} ]
+        };
+        setTimeout(callback, 0, null, result);
+      }
+    };
+
+    const lookupStream = stream(resolver);
+    t.doesNotThrow(() => {
+      test_stream([inputDoc], lookupStream, (err, actual) => {
+        t.deepEqual(actual, [expectedDoc], 'locality should be replaced with postal city');
+        t.end();
+      });
+    });
+  });
+
+  test.test('postal cities - AUS lookup - postcode 2612', (t) => {
+    const inputDoc = new Document( 'whosonfirst', 'placetype', '1')
+      .setCentroid({ lat: 12.121212, lon: 21.212121 })
+      .setAddress('zip', '2612');
+
+    const expectedDoc = new Document( 'whosonfirst', 'placetype', '1')
+      .setCentroid({ lat: 12.121212, lon: 21.212121 })
+      .addParent( 'country', 'Australia', '1', 'AUS')
+      .addParent( 'locality', 'Campbell', '101938727', undefined)
+      .addParent( 'locality', 'Braddon', '101938719', undefined)
+      .addParent( 'locality', 'Canberra', '1141909401', undefined)
+      .addParent( 'locality', 'Turner', '101938909', undefined)
+      .setAddress('zip', '2612');
+
+    const resolver = {
+      lookup: (centroid, search_layers, callback) => {
+        const result = {
+          country: [ {id: 1, name: 'Australia', abbr: 'AUS'} ]
         };
         setTimeout(callback, 0, null, result);
       }
