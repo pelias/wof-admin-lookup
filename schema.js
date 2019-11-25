1dd7718edd404ca5e5e3f41fdbf311756066d957
Julian Simioni <julian@simioni.org>
2019-11-25 11:11:37 -0500

    chore(schema): Update validation for new version of Joi

--- schema.js
+++ schema.js
@@ -2,7 +2,7 @@ const Joi = require('@hapi/joi');
 const cpus = require('os').cpus;
 
 module.exports = Joi.object().keys({
-  imports: Joi.object().keys({
+  imports: Joi.object().required().keys({
     adminLookup: Joi.object().keys({
       // default maxConcurrentReqs to # of cpus/cores * 10
       maxConcurrentReqs: Joi.number().integer().default(cpus().length*10),
@@ -11,17 +11,17 @@ module.exports = Joi.object().keys({
       usePostalCities: Joi.boolean().default(false)
     }).unknown(true),
     whosonfirst: Joi.object().keys({
-      datapath: Joi.string(),
+      datapath: Joi.string().required(),
       importPlace: [
         Joi.number().integer(),
         Joi.array().items(Joi.number().integer())
       ],
-      sqlite: Joi.boolean().default(false).truthy('yes').falsy('no').insensitive(true)
-    }).requiredKeys('datapath').unknown(true),
+      sqlite: Joi.boolean().default(false).truthy('yes').falsy('no')
+    }).unknown(true),
     services: Joi.object().keys({
       pip: Joi.object().keys({
-        url: Joi.string()
-      }).requiredKeys('url').unknown(true)
+        url: Joi.string().required()
+      }).unknown(true)
     }).unknown(true)
   }).or('whosonfirst', 'services.pip').unknown(true)
-}).requiredKeys('imports').unknown(true);
+}).unknown(true);
