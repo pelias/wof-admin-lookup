9e9a8408c6378b09d68a775e89fceb4b83224cfa
missinglink <insomnia@rcpt.at>
2022-09-13 13:44:03 +0200

    feat(endonyms): ensure parent endonyms exist for all countries and megacities

---
+++ src/data/aliases/country-language-map.sql
@@ -0,0 +1,23 @@
+/*
+  this SQL script can be used to regenerate the 'country-language-map.json' file.
+
+  at time of writing the required 'whosonfirst-data-country-latest.db' file isn't
+  distributed by Geocode Earth, you can generate it manually from the bundle file:
+
+  > aria2c https://data.geocode.earth/wof/dist/legacy/whosonfirst-data-country-latest.tar.bz2
+  > npm i @whosonfirst/wof
+  > wof bundle export whosonfirst-data-country-latest.tar.bz2 | wof sqlite import whosonfirst-data-country-latest.db
+*/
+
+SELECT json_group_object(id, json)
+FROM (
+  SELECT *, json_object(
+    'id', id,
+    'placetype', json_extract(body, '$.properties.wof:placetype'),
+    'name', json_extract(body, '$.properties.wof:name'),
+    'spoken', IFNULL(json_extract(body, '$.properties.wof:lang_x_spoken'), json_array()),
+    'official', IFNULL(json_extract(body, '$.properties.wof:lang_x_official'), json_array())
+  ) AS json
+  FROM geojson
+  WHERE json_extract(body, '$.properties.mz:is_current') == 1
+);
---
+++ src/data/aliases/extract-aliases.js
@@ -0,0 +1,60 @@
+const _ = require('lodash');
+const { stream, whosonfirst } = require('@whosonfirst/wof');
+
+/**
+ * this script can be used to generate '*-endonyms.psv' files from
+ * whosonfirst bundle files.
+ *
+ * note: I've not included the dependencies in package.json since
+ * this script is seldom run: npm install @whosonfirst/wof
+ */
+
+// reduce the language map to a key/value dict where the key
+// is the wofid and the value is an array containing all unique language tags.
+const languageMap = _.pickBy(
+  _.mapValues(require('./country-language-map'), (config) => {
+    return _.union(/*config.spoken, */config.official);
+  }),
+  _.negate(_.isEmpty)
+);
+
+// extract endonyms from target bundle file
+const BUNDLE_FILE = '/data/wof/whosonfirst-data-country-latest.tar.bz2';
+// const BUNDLE_FILE = '/data/wof/whosonfirst-data-locality-latest.tar.bz2'
+
+stream.bsdtar.extract('-f', BUNDLE_FILE, '--include', '*.geojson', '--exclude', '*-alt-*')
+  .pipe(stream.json.parse())
+  .pipe(stream.miss.through.obj((feature, enc, next) => {
+    if (!whosonfirst.feature.isCurrent()) { return next(); }
+    if (whosonfirst.feature.isAltGeometry()) { return next(); }
+    // if (_.get(feature, 'properties.wof:megacity') !== 1) { return next() }
+
+    // find which country the feature belongs to
+    const countryID = _.get(feature, 'properties.wof:hierarchy[0].country_id');
+    if (!countryID) { return next(); }
+
+    // find all endonyms for the feature
+    // ie. place names in all official local languages
+    const endonyms = _.pickBy(
+      _.get(feature, 'properties'),
+      (v, k) => {
+        // if (k === `name:eng_x_preferred`) { return true }
+        const matches = k.match(/name:([a-z]{3})_x_preferred/);
+        if (!matches) { return false; }
+        return _.get(languageMap, countryID, []).includes(matches[1]);
+      }
+    );
+    if (_.isEmpty(endonyms)) { return next(); }
+
+    // whosonfirst can be verbose, so only select the first toponym
+    // per language. ie. there can be at most one alias per language code.
+    const flattened = _.uniq(_.values(_.mapValues(endonyms, _.first)));
+
+    // find WOF ID
+    const wofID = whosonfirst.feature.getID(feature);
+
+    // write out pipe-seperated list
+    console.log(_.concat(wofID, _.sortBy(flattened)).join('|'));
+
+    next();
+  }));
