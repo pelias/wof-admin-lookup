f9af32a7a66966874daffed7af5760b9c2163ae3
Michael Kirk <michael.code@endoftheworl.de>
2023-12-12 12:32:29 -0800

    move away from event-stream, replace with stream-mock
    Part of pelias/pelias#801
    Technique cribbed from pelias/openaddresses#457

--- test/pip/components/filterOutCitylessNeighbourhoodsTest.js
+++ test/pip/components/filterOutCitylessNeighbourhoodsTest.js
@@ -1,15 +1,16 @@
 const tape = require('tape');
-const event_stream = require('event-stream');
+const stream_mock = require('stream-mock');
 const async = require('async');
 const proxyquire = require('proxyquire').noCallThru();
 
 const filterOutCitylessNeighbourhoods = require('../../../src/pip/components/filterOutCitylessNeighbourhoods');
 
 function test_stream(input, testedStream, callback) {
-    const input_stream = event_stream.readArray(input);
-    const destination_stream = event_stream.writeArray(callback);
-
-    input_stream.pipe(testedStream).pipe(destination_stream);
+  const reader = new stream_mock.ObjectReadableMock(input);
+  const writer = new stream_mock.ObjectWritableMock();
+  writer.on('error', (e) => callback(e));
+  writer.on('finish', () => callback(null, writer.data));
+  reader.pipe(testedStream).pipe(writer);
 }
 
 tape('non-neighbourhoods tests', test => {
