f45fe6eed7b515635eb34038ce7b3bff57d4b3dc
Peter Johnson <insomnia@rcpt.at>
2025-09-23 14:51:22 +0200

    refactor(spatial): use piscina module to simplify worker pool

--- src/spatialPipResolver.js
+++ src/spatialPipResolver.js
@@ -1,95 +1,30 @@
-const path = require('node:path');
-const { Worker } = require('worker_threads');
-const os = require('os');
+const path = require('path');
+const cpus = require('os').cpus().length;
+const { Piscina, FixedQueue } = require('piscina');
 
 class SpatialPipService {
-  constructor (datapath, layers, options = {}) {
-    this.datapath = datapath;
-    this.layers = layers;
-    this.workers = [];
-    this.currentWorker = 0;
-    this.messageId = 0;
-    this.pendingRequests = new Map();
-    
-    const numWorkers = options.numWorkers || os.cpus().length -1;
-    console.error(`Initializing ${numWorkers} spatial workers`);
-    
-    for (let i = 0; i < numWorkers; i++) {
-      this.createWorker();
-    }
+  constructor (datapath) {
+    this.pool = new Piscina({
+      filename: path.resolve(__dirname, 'spatialWorker.js'),
+      workerData: {
+        dbpath: path.resolve(datapath, '..', 'spatial', 'whosonfirst-data-admin-us-latest.spatial.db')
+      },
+      minThreads: cpus -1,
+      maxThreads: cpus -1,
+      idleTimeout: Infinity,
+      maxQueue: cpus * 5,
+      taskQueue: new FixedQueue()
+    });
   }
 
-  createWorker() {
-    const worker = new Worker(path.join(__dirname, 'spatialWorker.js'), {
-      workerData: { datapath: this.datapath }
-    });
-    
-    worker.on('message', (message) => {
-      const { id, type, data } = message;
-      const pendingRequest = this.pendingRequests.get(id);
-      
-      if (pendingRequest) {
-        this.pendingRequests.delete(id);
-        
-        if (type === 'result') {
-          pendingRequest.callback(null, data);
-        } else if (type === 'error') {
-          const error = new Error(data.message);
-          error.stack = data.stack;
-          pendingRequest.callback(error);
-        }
-      }
-    });
-    
-    worker.on('error', (error) => {
-      console.error('Worker error:', error);
-    });
-    
-    worker.on('exit', (code) => {
-      if (code !== 0) {
-        console.error(`Worker stopped with exit code ${code}`);
-      }
-    });
-    
-    this.workers.push(worker);
-  }
-
-  getNextWorker() {
-    const worker = this.workers[this.currentWorker];
-    this.currentWorker = (this.currentWorker + 1) % this.workers.length;
-    return worker;
-  }
-
-  lookup(centroid, search_layers, callback) {
-    try {
-      const messageId = ++this.messageId;
-      this.pendingRequests.set(messageId, { callback });
-      
-      const worker = this.getNextWorker();
-      worker.postMessage({
-        id: messageId,
-        type: 'lookup',
-        data: {
-          centroid,
-          // search_layers
-        }
-      });
-    } catch (error) {
-      console.error('Lookup error:', error);
-      callback(error);
-    }
+  lookup(centroid, _search_layers, cb) {
+    this.pool.run({ centroid })
+      .then(result => cb(null, result))
+      .catch(error => cb(error));
   }
 
   end() {
-    console.error('Terminating spatial workers');
-    
-    this.workers.forEach(worker => {
-      worker.postMessage({ type: 'close' });
-      worker.terminate();
-    });
-    
-    this.workers = [];
-    this.pendingRequests.clear();
+    this.pool.close();
   }
 }
 
--- src/spatialWorker.js
+++ src/spatialWorker.js
@@ -1,93 +1,29 @@
-const { parentPort, workerData, threadId } = require('worker_threads');
-const path = require('node:path');
+const { workerData } = require('piscina');
+const { dbpath: filename } = workerData;
+
 const QueryService = require('pelias-spatial/service/QueryService.js');
 const spatial = require('pelias-spatial/server/routes/pip_pelias.js');
 
-class SpatialWorker {
-  constructor(datapath) {
-    const filename = path.resolve(datapath, '..', 'spatial', 'whosonfirst-data-admin-us-latest.spatial.db');
-    console.error(`Worker ${threadId}: opening spatial database ${filename}`);
-    
-    this.service = new QueryService({
-      readonly: true,
-      filename
-    });
-  }
+console.error(`Worker: opening spatial database ${filename}`);
+const service = new QueryService({ readonly: true, filename });
 
-  lookup(centroid, search_layers) {
-    return new Promise((resolve, reject) => {
-      try {
-        const req = {
-          app: { locals: { service: this.service } },
-          params: {
-            lon: centroid.lon,
-            lat: centroid.lat
-          },
-          query: {
-            layers: (search_layers || []).join(',')
-          }
-        };
+module.exports = async function lookup({ centroid }) {
+  const req = {
+    app: { locals: { service } },
+    params: centroid,
+    query: { layers: '' }
+  };
 
-        const res = {
-          status: (_code) => res,
-          json: (result) => resolve(result)
-        };
+  return new Promise((resolve, reject) => {
+    const res = {
+      status: (_code) => res,
+      json: (result) => resolve(result)
+    };
 
-        spatial(req, res);
-      } catch (error) {
-        reject(error);
-      }
-    });
-  }
-
-  close() {
-    if (this.service && this.service.db) {
-      this.service.db.close();
-    }
-  }
-}
-
-let worker = null;
-
-if (parentPort) {
-  worker = new SpatialWorker(workerData.datapath);
-  
-  parentPort.on('message', async (message) => {
-    const { id, type, data } = message;
-    
     try {
-      switch (type) {
-        case 'lookup':
-          const result = await worker.lookup(data.centroid, data.search_layers);
-          parentPort.postMessage({ id, type: 'result', data: result });
-          break;
-          
-        case 'close':
-          worker.close();
-          parentPort.postMessage({ id, type: 'closed' });
-          process.exit(0);
-          break;
-          
-        default:
-          throw new Error(`Unknown message type: ${type}`);
-      }
+      spatial(req, res);
     } catch (error) {
-      parentPort.postMessage({ 
-        id, 
-        type: 'error', 
-        data: { 
-          message: error.message, 
-          stack: error.stack 
-        } 
-      });
+      reject(error);
     }
   });
-  
-  process.on('exit', () => {
-    if (worker) {
-      worker.close();
-    }
-  });
-}
-
-module.exports = SpatialWorker;
+};
